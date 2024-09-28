bf27175e78fc27eef11e76e9468cd88e1b1b1fd1
amartinez102015 <110202568+AleMarAlv2018133224@users.noreply.github.com>
2024-09-27 18:09:48 -0600

    Update tb_drv_mnt.sv

--- Nuevo/tb_drv_mnt.sv
+++ Nuevo/tb_drv_mnt.sv
@@ -7,34 +7,39 @@ module test_driver_monitor;
 
     //senales
     Driver_Monitor #( .drvrs(drvrs), .pckg_sz(pckg_sz)) dm;
+    bus_transaction transaction;
+
     always #5 clk = ~clk;
 
     initial begin
         clk=0;
         dm = new(0); 
-        clk = 0;
         fork
             dm.run();  
         join_none
 
-        for (int i = 0; i < 10; i++) begin  // Envía 10 transacciones aleatorias
-            dm.transaction = new();
-            dm.transaction.tipo = $urandom_range(0, 2);
-            dm.transaction.paquete= $urandom_range(0, 255);  
-            dm.agnt_drvr_mbx.put(dm.transaction);
+        for (int i = 0; i < 10; i++) begin  
+            transaction = new();
+            transaction.tipo = 1
+            transaction.paquete= $urandom_range(0, 255);  
+            dm.agnt_drvr_mbx.put(transaction);
 
-            $display("[%g] Agente envió transacción: Tipo=%0d, Paquete=0x%h", $time, dm.transaction.tipo, dm.transaction.paquete);
+            $display("[%g] Agente envió transacción: Tipo=%0d, Paquete=0x%h", $time, transaction.tipo, transaction.paquete);
             #10;
+
+            assert(dm.fifo_in.size() == (i + 1)) else 
+                $fatal("Error: FIFO de entrada no contiene el número esperado de datos.");
         end
         #50;
-        for (int j = 0; j < 10; j++) begin
-            if (dm.drv_chkr_mbx.get(dm.transaction)) begin // Intenta obtener una transacción del mailbox del checker
+        $finish;
+        //for (int j = 0; j < 10; j++) begin
+           // if (dm.drv_chkr_mbx.get(dm.transaction)) begin // Intenta obtener una transacción del mailbox del checker
                 // Imprime la transacción recibida en el checker
-                $display("[%g] Checker recibió transacción: Tipo=%0d, Paquete=0x%h", $time, transaction.tipo, transaction.paquete);
-            end else begin
-                $display("[%g] Checker no recibió transacción en el mailbox", $time);
-            end
-        end
+                //$display("[%g] Checker recibió transacción: Tipo=%0d, Paquete=0x%h", $time, transaction.tipo, transaction.paquete);
+           // end else begin
+                //$display("[%g] Checker no recibió transacción en el mailbox", $time);
+            //end
+        //end
         // Finaliza la simulación
     end
 
