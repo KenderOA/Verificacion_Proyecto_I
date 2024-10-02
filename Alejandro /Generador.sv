aaa3ccbddc47c43c5c09ec6a03009e654f35e52b
amartinez102015 <110202568+AleMarAlv2018133224@users.noreply.github.com>
2024-10-02 11:15:11 -0600

    generador sin chk

--- Alejandro /Generador.sv
+++ Alejandro /Generador.sv
@@ -6,14 +6,10 @@ class Generador #(parameter drvrs = 4, parameter pckg_sz = 16);
     gen_agnt_mbx        gen_agnt_mbx;
     gen_agnt            gen_agnt_transaction;
 
-    //gen_chk_sb_mbx      gen_chk_sb_mbx;
-    //gen_chk_sb          gen_chk_sb_transaction;
-
     function new();
     
         this.gen_agnt_transaction = new();
         this.tst_gen_transaction = new();
-        this.gen_chk_sb_transaction = new();
 
     endfunction
 
@@ -31,8 +27,6 @@ class Generador #(parameter drvrs = 4, parameter pckg_sz = 16);
                     this.gen_agnt_transaction.dis_src_rand       = 1;
                     this.gen_agnt_transaction.dis_src            = tst_gen_transaction.dis_src;
                     gen_agnt_mbx.put(gen_agnt_transaction);
-                    gen_chk_sb_transaction.cant_datos            = this.gen_agnt_transaction.cant_datos;
-                    gen_chk_sb_mbx.put(gen_chk_sb_transaction);
                     
                 end
                 brodcast:begin
@@ -44,8 +38,7 @@ class Generador #(parameter drvrs = 4, parameter pckg_sz = 16);
                     this.gen_agnt_transaction.dis_src_rand       = 1;
                     this.gen_agnt_transaction.dis_src            = tst_gen_transaction.dis_src;
                     gen_agnt_mbx.put(gen_agnt_transaction);
-                    gen_chk_sb_transaction.cant_datos            = this.gen_agnt_transaction.cant_datos;
-                    gen_chk_sb_mbx.put(gen_chk_sb_transaction);
+                    
                 end
                 one_to_all:begin
                     this.gen_agnt_transaction.tipo_data         = max_aleatoriedad;
@@ -56,8 +49,7 @@ class Generador #(parameter drvrs = 4, parameter pckg_sz = 16);
                     this.gen_agnt_transaction.dis_src_rand       = 0;
                     this.gen_agnt_transaction.dis_src            = tst_gen_transaction.dis_src;
                     gen_agnt_mbx.put(gen_agnt_transaction);
-                    gen_chk_sb_transaction.cant_datos            = this.gen_agnt_transaction.cant_datos;
-                    gen_chk_sb_mbx.put(gen_chk_sb_transaction);      
+                    
                 end
                 all_to_one:begin
                     this.gen_agnt_transaction.tipo_data         = max_aleatoriedad;
@@ -68,15 +60,11 @@ class Generador #(parameter drvrs = 4, parameter pckg_sz = 16);
                     this.gen_agnt_transaction.dis_src_rand       = 1;
                     this.gen_agnt_transaction.dis_src            = tst_gen_transaction.dis_src;
                     gen_agnt_mbx.put(gen_agnt_transaction);
-                    gen_chk_sb_transaction.cant_datos            = this.gen_agnt_transaction.cant_datos;
-                    gen_chk_sb_mbx.put(gen_chk_sb_transaction);
                     
                 end
                 
                 default:begin
-                    this.gen_agnt_transaction.cant_datos         = 10;
-                    gen_agnt_mbx.put(gen_agnt_transaction);
-                    gen_chk_sb_transaction.put(gen_chk_sb_transaction);
+                    this.gen_agnt_transaction.cant_datos         = 10;;
                 end
             endcase
         end
