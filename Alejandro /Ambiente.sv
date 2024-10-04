e51ad5a223205844b1d74d03c703bd183ed1270a
amartinez102015 <110202568+AleMarAlv2018133224@users.noreply.github.com>
2024-10-03 19:07:28 -0600

    Update Ambiente.sv

--- Alejandro /Ambiente.sv
+++ Alejandro /Ambiente.sv
@@ -2,16 +2,20 @@ class Ambiente #(parameter drvrs = 4, parameter pckg_sz = 16);
   
   // Declaración de los componentes del ambiente
   
-  Driver_Monitor #(.drvrs(drvrs), .pckg_sz(pckg_sz)) driver_inst[drvrs];
+  virtual bus_intf #(.drvrs(drvrs), .pckg_sz(pckg_sz)) bus_intf;
+  
+  Driver#(.drvrs(drvrs), .pckg_sz(pckg_sz)) driver_inst[drvrs];
+  Monitor #(.drvrs(drvrs), .pckg_sz(pckg_sz)) monitor_inst[drvrs];
   Agente #(.drvrs(drvrs), .pckg_sz(pckg_sz)) agente_inst;
   Generador #(.drvrs(drvrs), .pckg_sz(pckg_sz)) generador_inst;
   Checker_Scoreboard #(.drvrs(drvrs), .pckg_sz(pckg_sz)) checker_inst;
+  
 
   // Declaración de los mailboxes
   
-  bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) agnt_drv_mbx [drvrs]; 
-  bus_mbx  mnt_chkr_sb_mbx [drvrs];
-  bus_mbx drvr_chkr_sb_mbx [drvrs];
+  bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) agnt_drvr_mbx [drvrs]; 
+  bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) mnt_chkr_sb_mbx [drvrs];
+  bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) drvr_chkr_sb_mbx [drvrs];
   gen_agnt_mbx gen_agnt_mbx;
   tst_gen_mbx tst_gen_mbx; // mailbox del driver al checker
 
@@ -21,7 +25,7 @@ class Ambiente #(parameter drvrs = 4, parameter pckg_sz = 16);
     tst_gen_mbx = new();
     for (int i = 0; i < drvrs; i++) begin
       automatic int k = i;
-      this.agnt_drv_mbx[k] = new();
+      this.agnt_drvr_mbx[k] = new();
       this.mnt_chkr_sb_mbx[k] = new(); // Descomentar si es necesario
       this.drvr_chkr_sb_mbx[k] = new();
     end
@@ -40,11 +44,13 @@ class Ambiente #(parameter drvrs = 4, parameter pckg_sz = 16);
     
     for (int i = 0; i < drvrs; i++) begin
       automatic int k = i;
-      this.driver_inst[k] = new(k, bus_intf, k); // Instanciación del driver
-      this.driver_inst[k].agnt_drvr_mbx = agnt_drv_mbx[k];
-      this.driver_inst[k].mnt_chkr_sb_mbx = mnt_chkr_sb_mbx[k]; // Descomentar si es necesario
+      this.driver_inst[k] = new(k, bus_intf); // Instanciación del driver
+      this.monitor_inst[k]=new(k, bus_intf);
+      this.agente_inst.bus_mbx_array[k] = agnt_drvr_mbx[k];
+      this.driver_inst[k].agnt_drvr_mbx = agnt_drvr_mbx[k];
+      this.monitor_inst[k].mnt_chkr_sb_mbx = mnt_chkr_sb_mbx[k]; 
       this.driver_inst[k].drvr_chkr_sb_mbx = drvr_chkr_sb_mbx[k];
-      this.agente_inst.bus_mbx_array[k] = agnt_drv_mbx[k];  
+  
     end
   endfunction
 
@@ -55,11 +61,14 @@ class Ambiente #(parameter drvrs = 4, parameter pckg_sz = 16);
       agente_inst.run();
       for (int i = 0; i < drvrs; i++) begin
         automatic int k = i;
-        this.driver_inst[k].run();
+        
+        this.driver_inst[k].run_driver();
+        this.monitor_inst[k].run_monitor();
+        
       end
-      checker_inst.run_drvr(); // Descomentar si es necesario
-      chekcer_inst.run_mnt();
-      chekcer_inst.report_sb();
+      //checker_inst.run_mnt();
+      //checker_inst.run_drvr();
+      //void'(checker_inst.report_sb());
     join_none;
   endtask
 endclass
