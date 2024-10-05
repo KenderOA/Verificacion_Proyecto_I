f75f1f6c527d632ebe58e02c0ba67ab1264bf645
amartinez102015 <110202568+AleMarAlv2018133224@users.noreply.github.com>
2024-10-04 22:31:43 -0600

    Update Agente.sv

--- Alejandro /Agente.sv
+++ Alejandro /Agente.sv
@@ -1,48 +1,55 @@
 class Agente #(parameter drvrs = 4, parameter pckg_sz = 16);
 
-    bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz))   bus_mbx_array [drvrs]; //agnt_drvr_mbx_array   //Arreglo de mailboxes
-    bus_transaction #(.drvrs(drvrs), .pckg_sz(pckg_sz))       bus_transaction; //agnt_drvr_transaction         //Transacción
+    bus_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz))                 bus_mbx_array [drvrs]; 
+    bus_transaction #(.drvrs(drvrs), .pckg_sz(pckg_sz))         bus_transaction; 
 
-    gen_agnt_mbx                                        gen_agnt_mbx;                   //Mailbox
-    gen_agnt                                            gen_agnt_transaction;           //Transacción
+    gen_agnt_mbx                                                gen_agnt_mbx;                  
+    gen_agnt                                                    gen_agnt_transaction;           
 
     int num_transacciones;
-    //int tmp_ret; //delay
-    int fuente; //dis_src
+    int fuente; 
 
     function new();
 
     this.bus_transaction = new();
 
         for (int i = 0; i < drvrs; i++) begin
-            this.bus_mbx_array[i] = new(); // Cambio de chatgpt
+          automatic int k = i;
+          this.bus_mbx_array[k] = new(); // Cambio de chatgpt
         end
 
-        //for (int i = 0; i < drvrs; i++) begin
-            //temp int index = i;                                        //Variable temporal
-            //this.bus_mbx_array[index] = new();                   //Inicialización de mailboxes
-        //end
     endfunction
     
     task run();
-      	$display("Agente inicializado");
-        forever begin
-            this.gen_agnt_mbx.get(this.gen_agnt_transaction);               
-            this.num_transacciones = this.gen_agnt_transaction.cant_datos;
-            for (int i = 0; i < this.num_transacciones; i++) begin
 
+      	$display("[%g] ++++++++++++++ EL AGENTE FUE INICIALIZADO ++++++++++++++",$time);
+
+        forever begin
+            this.gen_agnt_mbx.get(this.gen_agnt_transaction);  
+            $display("[%g] TRANSACCIÓN RECIBIDA GENERADOR => AGENTE: %h", $time, this.gen_agnt_transaction);            
+            this.num_transacciones = this.gen_agnt_transaction.cant_datos;
+            $display("[%g] NÚMERO DE TRANSACCIONES: %d", $time, this.num_transacciones); 
+
+            for (int i = 0; i < this.num_transacciones; i++) begin
+              
+				this.bus_transaction = new();
                 case (this.gen_agnt_transaction.tipo_data)
-                    //this.bus_transaction = new(); //no se si ponerlo
-                    max_variabilidad: bus_transaction.dato_valido.constraint_mode(1);        //Modo de variabilidad
-                    max_aleatoriedad: bus_transaction.dato_valido.constraint_mode(0);         //Modo de aleatoriedad
-                    default: bus_transaction.dato_valido.constraint_mode(0);                //Modo por defecto
+                    max_variabilidad:begin
+                      bus_transaction.dato_valido.constraint_mode(1);
+                    end
+                    max_aleatoriedad:begin
+                      bus_transaction.dato_valido.constraint_mode(0);
+                    end
+                    default: begin 
+                      bus_transaction.dato_valido.constraint_mode(0);
+                    end
                 endcase
 
                 case (this.gen_agnt_transaction.tipo_id)
                     self_id: begin
-                        bus_transaction.fuente_destino.constraint_mode(0);                            //Modo de auto identificación
-                        bus_transaction.id_valida.constraint_mode(1);                           //Modo de dirección válida
-                        bus_transaction.fuente_valida.constraint_mode(1);                          //Modo de dirección de origen
+                        bus_transaction.fuente_destino.constraint_mode(0);                       
+                        bus_transaction.id_valida.constraint_mode(1);                        
+                        bus_transaction.fuente_valida.constraint_mode(1);                       
                         bus_transaction.dispositivo_valido.constraint_mode(1);                      
                     end
                     any_id: begin
@@ -75,7 +82,7 @@ class Agente #(parameter drvrs = 4, parameter pckg_sz = 16);
                     end
                 endcase
 
-                this.bus_transaction.randomize(); //no sé si ponerlo
+                this.bus_transaction.randomize();
 
                 if (this.gen_agnt_transaction.dis_src_rand== 0) begin
                     bus_transaction.dis_src = gen_agnt_transaction.dis_src;
@@ -96,11 +103,11 @@ class Agente #(parameter drvrs = 4, parameter pckg_sz = 16);
 
                 this.bus_transaction.tiempo= $time;
                 this.bus_mbx_array[this.bus_transaction.dis_src].put(this.bus_transaction);
-              		$display("Tamaño de bus_mbx_array: %d", $size(this.bus_mbx_array));
-                         $display("Dispositivo fuente: %d",this.bus_transaction.dis_src);
-              				$display("Dispositivo destino: %d",this.bus_transaction.id);
-              					$display("Transaccion: %d",this.bus_transaction.paquete);
-
+                $display("[%g] TRANSACCIÓN ENVIADA AGENTE => DRIVER: %h", $time, this.bus_transaction);
+              	$display("[%g] PAQUETE A ENVIAR:    %d",$time, this.bus_transaction.paquete); 
+              	$display("[%g] DISPOSITIVO FUENTE:  %d",$time, this.bus_transaction.dis_src);
+             	$display("[%G] DISPOSITIVO DESTINO: %d",$time, this.bus_transaction.id);
+              	
             end
         end
     endtask
